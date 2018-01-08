1c6077e79a9fa590c8bc1477fcf5a0f3a0743fb7
Jie Zhou <jzhou41@cs.rochester.edu>
2018-01-07 23:41:18 -0500

    continue working on compressing call graph

--- CompressCallGraph.cpp
+++ CompressCallGraph.cpp
@@ -12,12 +12,15 @@
  *  2. reachable from the source function in the original call graph. 
  * */
 
+
 #include "CompressCallGraph.h"
-#include "LocalAnalysis.h"
+#include <algorithm>
 
 using namespace llvm;
 using namespace compressCG;
+using namespace privCallGraph;
 using namespace localAnalysis;
+using namespace privADT;
 
 // constructor
 CompressCG::CompressCG() : ModulePass(ID) {}
@@ -27,55 +30,94 @@ CompressCG::CompressCG() : ModulePass(ID) {}
  * */
 void CompressCG::getAnalysisUsage(AnalysisUsage &AU) const {
     // run LocalAnalysis first
-    /* AU.addRequired<LocalAnalysis>(); */
+    AU.addPreserved<LocalAnalysis>();
+    AU.addRequired<LocalAnalysis>();
 
     // doesn't transform program
     AU.setPreservesAll();
 }
 
 
-// runOnModule
+/*
+ * runOnModule: entrance of this Module Pass
+ * */
 bool CompressCG::runOnModule(Module &M) {
     errs() << "Hello from CompressCG Pass!\n";
 
-    debugHelper(M);
+    /* errs() << "\nFunctions from CompressCG:\n"; */
+    /* for (auto i = M.begin(); i != M.end(); i++) errs() << i->getName() << ": " << &*i << "\n"; */
+
+    LocalAnalysis &LA = getAnalysis<LocalAnalysis>();
+    funcCapMap = LA.FuncCAPTable;
+    bbCapMap = LA.BBCAPTable;
+
+    /* errs() << "\nFunctions from LocalAnalysis:\n"; */
+    /* for (auto i = LA.funcs.begin(); i != LA.funcs.end(); i++) errs() << (*i)->getName() << ": " << *i << "\n"; */
+    /* errs() << "\n"; */
+    
+    // build the original call graph
+    privCG = new PrivCallGraph(M);
+    privCG->print();
+
+    // remove unreachable functions and blocks
+    removeUnreachableFuncs(privCG);
+    
+    /* debugHelper(M); */
     return false;
 }
 
+/*
+ * removeUnreachableFuncs() removes unreachable functions from funcCapMap.
+ * It also remove basic blocks in those deleted functions, though the function name omits that.
+ *
+ * Actually, we don't need call this function to get correct a result for the whole analysis...
+ * */
+void CompressCG::removeUnreachableFuncs(PrivCallGraph *privCG) {
+    // remove functions
+    for (auto fcmi = funcCapMap.begin(); fcmi != funcCapMap.end();) {
+        if (!privCG->hasFunction(fcmi->first)) funcCapMap.erase(fcmi++);
+        else fcmi++;
+    }
+
+    // remove basic blocks in the just deleted functions
+    for (auto bbcmi = bbCapMap.begin(); bbcmi != bbCapMap.end();) {
+        if (!privCG->hasFunction(bbcmi->first->getParent())) bbCapMap.erase(bbcmi++);
+        else bbcmi++;
+    }
+}
+
+
 void CompressCG::debugHelper(Module &M) const {
     /* printAllFunc(M); */
-    
-    CallGraph CG(M);
-    /* printCallGraph(M, CG); */
-    /* CG.print(errs()); */
-    CG.dump();
+
 }
+    
+// print all Function names and their addresses
 void CompressCG::printAllFunc(Module &M) const {
     errs() << "all functions:\n";
     for (Module::iterator mi = M.begin(); mi != M.end(); mi++) {
-        errs() << mi->getName() << "\n";
+        errs() << mi->getName() << ": " << &*mi << "\n";
     }
-        
 }
-void CompressCG::printCallGraph(Module &M, CallGraph &CG) const {
+void CompressCG::printCallGraph(Module &M, llvm::CallGraph &CG) const {
     /* errs() << "call relations:\n"; */
     /* errs() << "external nodes:\n"; */
     errs() << "print call graph nodes\n";
     for (auto mi = M.begin(); mi != M.end(); mi++) {
+        if (mi->getName().equals("main") == false) continue;
         errs() << mi->getName() << " gets called ";
-        CallGraphNode *cgn = CG[&*mi];
+        llvm::CallGraphNode *cgn = CG[&*mi];
         /* cgn->dump(); */
         errs() << cgn->getNumReferences() << " times: ";
-        CallGraphNode *callee = (*cgn)[1];
+        llvm::CallGraphNode *callee = (*cgn)[0];
         Function *f = callee->getFunction();
-        errs() << "====";
-        errs() << f->getName();
-        /* for (unsigned i = 0; i < cgn->getNumReferences(); i++) { */
-            /* errs() << ((*cgn)[0])->getFunction()->getName() <<" "; */
-        /* } */
+        if (f == NULL) {
+            errs() << "f is null\n";
+        }
         errs() << "\n";
     }
 }
+
 
 char CompressCG::ID = 0;
 static RegisterPass<CompressCG> A("compress_cg",  // name used as an arugment for opt
--- CompressCallGraph.h
+++ CompressCallGraph.h
@@ -15,17 +15,32 @@
 #include "llvm/IR/Instructions.h"
 #include "llvm/Analysis/CallGraph.h"
 #include "llvm/Support/raw_ostream.h"
+#include "PrivCallGraph.h"
+
+#include "LocalAnalysis.h"
+#include "ADT.h"
+#include <unordered_map>
 
 #ifndef COMPRESSED_CALL_GRAPH_H
 #define COMPRESSED_CALL_GRAPH_H
 
 using namespace llvm;
-/* using namespace privADT; */
+using namespace privCallGraph;
+using namespace localAnalysis;
+using namespace privADT;
 
 namespace compressCG {
 struct CompressCG : public ModulePass {
 public:
     static char ID;
+
+    // map from function to CAPArray
+    FuncCAPTable_t funcCapMap;
+    // map from basic block to CAPArray
+    BBCAPTable_t bbCapMap;
+
+    // map a fucntion F to functions that use some privilege and are reachable from F
+    std::unordered_map<Function *, PrivCallGraphNode *> reachablePrivFunc;
 
     // constructor
     CompressCG();
@@ -39,10 +54,16 @@ public:
     // to some pass before this one
     virtual void getAnalysisUsage(AnalysisUsage &AU) const;
 private:
-    void prijtAllFunc(Module &M) const;
-    void printCallGraph(Module &M, CallGraph &CG) const;
+    PrivCallGraph *privCG;
+
+    // remove unreachable functions from funcCapMap and bbCapMap
+    void removeUnreachableFuncs(PrivCallGraph *privCG);
+
+    void printAllFunc(Module &M) const;
+    void printCallGraph(Module &M, llvm::CallGraph &CG) const;
     void debugHelper(Module &M) const;
 };
-}
 
-#endif
+}  // end of compressCG namespace
+
+#endif  // COMPRESSED_CALL_GRAPH_H
